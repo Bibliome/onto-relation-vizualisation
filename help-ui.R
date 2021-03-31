a524ff35d6a6665867f25e6283fce22e53b5aedb
mouhamadou ba <mandiayba@gmail.com>
2021-03-31 14:02:09 +0200

    Create help-ui.R

---
+++ help-ui.R
@@ -0,0 +1,18 @@
+Help <- div(HTML("<p>
+                  Questions, problems or comments regarding this application should be sent to
+                  <a href = \"mailto:mouhamadou.ba@inrae.fr?subject=[Easy16S]\">mouhamadou.ba@inrae.fr</a>
+                  </p>
+
+                  <p>
+                  For more information about this tool, you can refer to
+                  <a href = \"https://github.com/Bibliome/onto-relation-vizualisation\">this poster</a>.
+                  </p>
+                  
+                  <p>
+                  <u>Cite us:</u>
+                  Research teams that have used this app are expected to thank the Migale bioinformatics facility in the publications of their analyses with the following sentence: <br>
+                  <i>\"We are grateful to the INRAE MIGALE bioinformatics facility (MIGALE, INRAE, 2020. Migale bioinformatics Facility, doi: 10.15454/1.5572390655343293E12) for providing help and/or computing and/or storage resources\"</i>
+                  </p>
+                  "
+  )
+)
